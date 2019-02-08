33c13a7c73119a3c17397f2d33809e68b88bcd70
David M. Hildebrandt <david.m.hildebrandt@gmail.com>
2019-02-08 08:14:17 -0500

    Starting to work with Futures and Akka client management.

--- src/main/scala/com.lantern.akka/AkkaStart.scala
+++ src/main/scala/com.lantern.akka/AkkaStart.scala
@@ -1,26 +1,68 @@
 package com.lantern.akka
 
 import scala.concurrent.Future
+import scala.util.{Failure, Random, Success}
 
 object AkkaStart {
 
   implicit val ec = scala.concurrent.ExecutionContext.global
 
+  def printMessage(delay: Int, message: String) = {
+    println(s"Thread[${Thread.currentThread().getName}] $message")
+  }
+
+  def createFuture(delay: Int): Future[Int] = Future {
+    printMessage(delay, s"Before Thread.sleep(${delay})")
+    Thread.sleep(delay)
+    if (Random.nextBoolean) throw new RuntimeException("Dead!")
+    printMessage(delay, s"After Thread.sleep(${delay})")
+    delay
+  }
+
   def main(a: Array[String]): Unit = {
-    
-    val f1 = Future[Int] {
-      println("Hello")
-      0
-    }
+    val DELAY_LIMIT = 1000
+
+    printMessage(0, "a")
+    val f1 = createFuture(Random.nextInt(DELAY_LIMIT))
+    printMessage(0, "b")
+    val f2 = createFuture(Random.nextInt(DELAY_LIMIT))
+    printMessage(0, "c")
+
+    val result = f1.flatMap(i => {
+      printMessage(0, "f1.flatMap")
+      f2.map(j => {
+        printMessage(0, "f2.map")
+       i + j})
+    })
 
     for {
-      i <- f1
+      v1 <- f1
+      v2 <- f2
     } {
-      println(s"got i $i")
+      println(s"v1 + v2 = ${v1 + v2}")
     }
 
+    handleFuture(f1)
+    handleFuture(f2)
+    handleFuture(result)
+
+    // As long as the main call stack remains in memory
+    // the child processes in the Future objects will
+    // complete.  The main Thread is being blocked during this
+    // time.
+    while (!f1.isCompleted && !f2.isCompleted) {
+      Thread.sleep(DELAY_LIMIT * 2)
+    }
+
+    printMessage(0, "exit")
 
   }
 
+  def handleFuture(f: Future[Int]): Unit = {
+    f.onComplete {
+      case Success(i) => println(s"Success: ${i}")
+      case Failure(e) => println(s"Failure: ${e}")
+    }
+  }
 
 }
